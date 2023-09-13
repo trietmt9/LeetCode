947b990d64d16d84c3851e4cca37c0873a45779a
trietmt9 <tuo7hc@bosch.com>
2023-09-13 11:55:30 +0700

    Palindrome Number Done

---
+++ Palindrome_Number/Palindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, tmp, input;
+    printf("Insert a number: ");
+    scanf("%d", &n);
+    int res = 0;
+    input = n; 
+    while(n > 0){
+        tmp = n % 10;
+        res = res * 10 + tmp;
+        n = n / 10;
+    }
+
+    if(res == input) printf("True ");
+    else printf("False ");
+}
